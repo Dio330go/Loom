06100d74ec9073c7f933c694337d89f1e7bbf52e
Diogo <lrosado.diogo@gmail.com>
2025-05-25 22:23:59 +0100

    post endpoint of appointments added

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
-class Date:
-    def __init__(self, hour, minute, day, month, year):
-        self.hour = hour
-        self.minute = minute
-        self.day = day
-        self.month = month
-        self.year = year
-
-    def __repr__(self):
-        return f"{self.year}{self.month:02d}{self.day:02d}{self.hour:02d}{self.minute:02d}"
-    
-    def __str__(self):
-        return f"{self.hour:02d}:{self.minute:02d} de {self.day:02d}/{self.month:02d}/{self.year}"
-
-class Appointment:
-    def __init__(self, name: str, date: Date):
-        self.name = name
-        self.date = date
+class Appointment(BaseModel):
+    name: str
+    date: datetime
 
     def __str__(self):
-        return f"Tens de ir {self.name} às {str(self.date)}"
-   
-def date_size(appointment:Appointment):
-    return int(repr(appointment.date))
-    
+        return f"Tens de ir {self.name} às {self.date.strftime('%H:%M')} a {self.date.strftime('%d/%m/%Y')}"
+      
 appointments = [
-    Appointment("Urologista", Date(23, 5, 3, 5, 2025)), 
-    Appointment("Putas", Date(0, 0, 13, 12, 2025)),
-    Appointment("Derrubar Império Romano", Date(13, 0, 1, 1, 2032)),
-    Appointment("fazer sexo com o mesquita", Date(22, 0, 24, 5, 2025))
+    Appointment(name="Urologista", date=datetime(2025, 5, 3, 23, 5)), 
+    Appointment(name="Putas", date=datetime(2025, 12, 13, 0, 0)),
+    Appointment(name="Derrubar Império Romano", date=datetime(2032, 1, 1, 13, 0)),
+    Appointment(name="fazer sexo com o mesquita", date=datetime(2025, 5, 24, 22, 0))
 ]
 
-appointments.sort(key=date_size)
+def sort_appointments():
+    def date_size(appointment:Appointment):
+        return appointment.date
+    appointments.sort(key=date_size)
+sort_appointments()
 
 app = FastAPI()
 
@@ -45,9 +33,14 @@ def see_appointments():
     return appointments
 
 @app.get("/appointments/{appointment_id}")
-def see_appointment(appointment_id: int):
+def see_appointment(appointment_id  : int):
     return str(appointments[appointment_id])
 
+@app.post("/appointments/")
+async def create_appointment(appointment: Appointment):
+    appointments.append(appointment)
+    sort_appointments()
+    return appointments
 
 #  ⠀⠖⠖⡆⠀⠀⠀⠀⣀⣀⣀⠀⠀
 #  ⢸⠀⠀⡗⠐⠉⠁⠀⠀⣇⡤⠽⡆
