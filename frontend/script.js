89a0af548bb9b92a3573653548f1d9c9d1b1bbd8
mig303 <miguelmesquita303mm@gmail.com>
2025-05-26 22:26:33 +0100

    stfu diogo

--- frontend/script.js
+++ frontend/script.js
@@ -20,15 +20,24 @@ function store_values(Name, Date) {
   .catch(error => console.error(error));// 6. Handle any errors
 }
 
+function open_new_appointment() {
+  document.getElementById("new_appointment").style.display = "block";
+}
+
+function close_new_appointment() {
+  document.getElementById("new_appointment").style.display = "none";
+}
+
 function submit() {
   const name = document.getElementById('name').value.trim();
   const date = document.getElementById('date').value;
+  const notes = document.getElementById('notes').value.trim();
   if (name !== "" && date !== "") {
     store_values(name, date)
     document.getElementById('name').value = '';
     document.getElementById('date').value = '';
     document.getElementById('notes').value = '';
-    document.getElementById('submit_confirmation').innerHTML = 'o teu apointment ja ta bue guardado';
+    document.getElementById('submit_confirmation').innerHTML = 'good boy';
     document.getElementById('submit_confirmation').style.color = '#1c9c3e';
   } else {
     document.getElementById('submit_confirmation').innerHTML = 'prenche bem essa merda';
