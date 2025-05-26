ae583e8055b0df4cfb45955877212c980f127468
Diogo <lrosado.diogo@gmail.com>
2025-05-26 22:21:47 +0100

    suporte para notes nos appointments

--- frontend/script.js
+++ frontend/script.js
@@ -4,27 +4,34 @@ function clean_msg() {
   document.getElementById('submit_confirmation').style.transition = "opacity 0.3s linear";
   document.getElementById('submit_confirmation').style.opacity = 0;
 }
-function store_values(Name, Date) {
+function store_values(Name, Date, Notes) {
+  if (Notes == '') {
+    Notes = null;
+  }
+  const appointment = {                  
+    name: Name,
+    date: Date,
+    notes: Notes,
+  }
+
   fetch('http://localhost:8000/appointments', {
-    method: 'POST',                        // 1. HTTP method: POST (can also be GET, PUT, DELETE, etc.)
+    method: 'POST',                       // 1. HTTP method: POST (can also be GET, PUT, DELETE, etc.)
     headers: {
       'Content-Type': 'application/json', // 2. Tells the server we're sending JSON
     },
-    body: JSON.stringify({                // 3. Convert a JS object to JSON
-      name: Name,
-      date: Date,
-    }),
+    body: JSON.stringify(appointment),    // 3. Convert a JS object to JSON
   })
-  .then(response => response.json())    // 4. Parse the response as JSON
-  .then(data => console.log(data))      // 5. Do something with the response
-  .catch(error => console.error(error));// 6. Handle any errors
+  .then(response => response.json())      // 4. Parse the response as JSON
+  .then(data => console.log(data))        // 5. Do something with the response
+  .catch(error => console.error(error));  // 6. Handle any errors
 }
 
 function submit() {
   const name = document.getElementById('name').value.trim();
   const date = document.getElementById('date').value;
+  const notes = document.getElementById('notes').value.trim();
   if (name !== "" && date !== "") {
-    store_values(name, date)
+    store_values(name, date, notes)
     document.getElementById('name').value = '';
     document.getElementById('date').value = '';
     document.getElementById('notes').value = '';
